aa9e68ce3946b8ee5aca4294254ecb305455ec98
Irinapreo <irinapreobrazhenskaya100@gmail.com>
2023-10-05 20:46:47 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.NetworkInformation;
+
+namespace IOfromTxtFile;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+
+        //string[] lines = File.ReadAllLines(filePath);
+
+        List<string> lines = new List<string>();
+        lines = File.ReadAllLines("../../../Test.txt").ToList();
+        foreach (var line in lines)
+        {
+            Console.WriteLine(line);
+        }
+
+        lines.Add("John, DOes, www.nobody.com");
+        File.WriteAllLines("../../../Test.txt", lines);
+
+        Console.ReadLine();
+
+    }
+}
+
